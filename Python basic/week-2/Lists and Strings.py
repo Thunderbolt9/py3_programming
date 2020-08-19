59684ecdeff13e711b6b12d9d0ede908dc387225
Bhavesh Yadav <63863797+Thunderbolt9@users.noreply.github.com>
2020-08-19 22:16:39 +0530

    Add files via upload

---
+++ Python basic/week-2/Lists and Strings.py
@@ -0,0 +1,75 @@
+'''Q-1: What will the output be for the following code?
+    let = "z"
+    let_two = "p"
+    c = let_two + let
+    m = c*5
+    print(m)'''
+#Answer :
+         # pzpzpzpzpz
+
+xyz = square(5)
+print(xyz)
+
+'''Q-2: Write a program that extracts the last three items in the list sports and assigns it to the variable last.
+Make sure to write your code so that it works no matter how many items are in the list.'''
+
+sports = ['cricket', 'football', 'volleyball', 'baseball', 'softball', 'track and field', 'curling', 'ping pong', 'hockey']
+last=sports[-3:]
+print(last)
+
+
+'''Q-3: Write code that combines the following variables so that the sentence “You are doing a great job, keep it up!” is assigned to the variable message.
+Do not edit the values assigned to by, az, io, or qy.'''
+
+by = "You are"
+az = "doing a great "
+io = "job"
+qy = "keep it up!"
+message=by+" "+az+io+","+" "+qy
+print(message)
+
+
+'''Q-4: What will the output be for the following code?
+    ls = ['run', 'world', 'travel', 'lights', 'moon', 'baseball', 'sea']
+    new = ls[2:4]
+    print(new)'''
+
+#Answer :
+         # ['travel', 'lights']
+
+
+'''Q-5: What is the type of m?
+    l = ['w', '7', 0, 9]
+    m = l[1:2]'''
+
+#Answer :
+         #  list
+
+
+'''Q-6: What is the type of m?
+    l = ['w', '7', 0, 9]
+    m = l[1]'''
+
+#Answer :
+         #  string
+
+
+'''Q-7: What is the type of x?
+    b = "My, what a lovely day"
+    x = b.split(',')'''
+
+#Answer :
+         #  list
+
+
+'''Q-8: What is the type of a?
+    b = "My, what a lovely day"
+    x = b.split(',')
+    z = "".join(x)
+    y = z.split()
+    a = "".join(y)'''
+
+#Answer :
+         #  string
+
+
