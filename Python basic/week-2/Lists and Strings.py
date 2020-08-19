f31174272ce9831e9109ac9de774cda13f33e839
Bhavesh Yadav <63863797+Thunderbolt9@users.noreply.github.com>
2020-08-19 22:58:36 +0530

    Update Lists and Strings.py

--- Python basic/week-2/Lists and Strings.py
+++ Python basic/week-2/Lists and Strings.py
@@ -7,8 +7,6 @@
 #Answer :
          # pzpzpzpzpz
 
-xyz = square(5)
-print(xyz)
 
 '''Q-2: Write a program that extracts the last three items in the list sports and assigns it to the variable last.
 Make sure to write your code so that it works no matter how many items are in the list.'''
