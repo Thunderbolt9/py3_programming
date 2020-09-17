4730d67e491497b12d0d71ad1bd6225300d59bd1
Bhavesh <63863797+Thunderbolt9@users.noreply.github.com>
2020-09-17 23:31:55 +0530

    Add files via upload

---
+++ Python Classes and Inheritance/week-2/Week two (2).py
@@ -0,0 +1,239 @@
+#Q-1.
+
+class Pokemon(object):
+    attack = 12
+    defense = 10
+    health = 15
+    p_type = "Normal"
+
+    def __init__(self, name, level = 5):
+        self.name = name
+        self.level = level
+
+    def train(self):
+        self.update()
+        self.attack_up()
+        self.defense_up()
+        self.health_up()
+        self.level = self.level + 1
+        if self.level%self.evolve == 0:
+            return self.level, "Evolved!"
+        else:
+            return self.level
+
+    def attack_up(self):
+        self.attack = self.attack + self.attack_boost
+        return self.attack
+
+    def defense_up(self):
+        self.defense = self.defense + self.defense_boost
+        return self.defense
+
+    def health_up(self):
+        self.health = self.health + self.health_boost
+        return self.health
+
+    def update(self):
+        self.health_boost = 5
+        self.attack_boost = 3
+        self.defense_boost = 2
+        self.evolve = 10
+
+    def __str__(self):
+        self.update()
+        return "Pokemon name: {}, Type: {}, Level: {}".format(self.name, self.p_type, self.level)
+
+class Grass_Pokemon(Pokemon):
+    attack = 15
+    defense = 14
+    health = 12
+
+    def update(self):
+        self.health_boost = 6
+        self.attack_boost = 2
+        self.defense_boost = 3
+        self.evolve = 12
+
+    def moves(self):
+        self.p_moves = ["razor leaf", "synthesis", "petal dance"]
+
+    def action(self):
+        return "{} knows a lot of different moves!".format(self.name)
+    
+p1 = Grass_Pokemon("Belle") 
+
+#Q-2.
+
+class Pokemon(object):
+    attack = 12
+    defense = 10
+    health = 15
+    p_type = "Normal"
+
+    def __init__(self, name, level = 5):
+        self.name = name
+        self.level = level
+
+    def train(self):
+        self.update()
+        self.attack_up()
+        self.defense_up()
+        self.health_up()
+        self.level = self.level + 1
+        if self.level%self.evolve == 0:
+            return self.level, "Evolved!"
+        else:
+            return self.level
+
+    def attack_up(self):
+        self.attack = self.attack + self.attack_boost
+        return self.attack
+
+    def defense_up(self):
+        self.defense = self.defense + self.defense_boost
+        return self.defense
+
+    def health_up(self):
+        self.health = self.health + self.health_boost
+        return self.health
+
+    def update(self):
+        self.health_boost = 5
+        self.attack_boost = 3
+        self.defense_boost = 2
+        self.evolve = 10
+
+    def __str__(self):
+        return "Pokemon name: {}, Type: {}, Level: {}".format(self.name, self.p_type, self.level)
+
+class Grass_Pokemon(Pokemon):
+    attack = 15
+    defense = 14
+    health = 12
+    p_type = "Grass"
+
+    def update(self):
+        self.health_boost = 6
+        self.attack_boost = 2
+        self.defense_boost = 3
+        self.evolve = 12
+        
+    def train(self):
+        self.update()
+        if self.level >= 10:
+            self.attack_up()
+        self.defense_up()
+        self.health_up()
+        self.level = self.level + 1
+        if self.level%self.evolve == 0:
+            return self.level, "Evolved!"
+        else:
+            return self.level
+
+    def moves(self):
+        self.p_moves = ["razor leaf", "synthesis", "petal dance"]
+
+
+p2 = Grass_Pokemon("Bulby", level = 5)
+p3 = Grass_Pokemon("Pika", level = 5)
+p3.train()
+
+#Q-3.
+
+class Pokemon():
+    attack = 12
+    defense = 10
+    health = 15
+    p_type = "Normal"
+
+    def __init__(self, name,level = 5):
+        self.name = name
+        self.level = level
+        self.weak = "Normal"
+        self.strong = "Normal"
+
+    def train(self):
+        self.update()
+        self.attack_up()
+        self.defense_up()
+        self.health_up()
+        self.level = self.level + 1
+        if self.level%self.evolve == 0:
+            return self.level, "Evolved!"
+        else:
+            return self.level
+
+    def attack_up(self):
+        self.attack = self.attack + self.attack_boost
+        return self.attack
+
+    def defense_up(self):
+        self.defense = self.defense + self.defense_boost
+        return self.defense
+
+    def health_up(self):
+        self.health = self.health + self.health_boost
+        return self.health
+
+    def update(self):
+        self.health_boost = 5
+        self.attack_boost = 3
+        self.defense_boost = 2
+        self.evolve = 10
+
+    def __str__(self):
+        self.update()
+        return "Pokemon name: {}, Type: {}, Level: {}".format(self.name, self.p_type, self.level)
+        
+    def opponent(self):
+        return self.weak, self.strong
+
+class Grass_Pokemon(Pokemon):
+    attack = 15
+    defense = 14
+    health = 12
+    p_type = "Grass"
+    
+    def __init__(self, name,level = 5):
+        self.name = name
+        self.level = level
+        self.weak = "Fire"
+        self.strong = "Water"
+
+    def update(self):
+        self.health_boost = 6
+        self.attack_boost = 2
+        self.defense_boost = 3
+        self.evolve = 12
+
+class Ghost_Pokemon(Pokemon):
+    p_type = "Ghost"
+    
+    def __init__(self, name,level = 5):
+        self.name = name
+        self.level = level
+        self.weak = "Dark"
+        self.strong = "Psychic"
+    
+    def update(self):
+        self.health_boost = 3
+        self.attack_boost = 4
+        self.defense_boost = 3
+
+class Fire_Pokemon(Pokemon):
+    p_type = "Fire"
+    
+    def __init__(self, name,level = 5):
+        self.name = name
+        self.level = level
+        self.weak = "Water"
+        self.strong = "Grass"    
+
+class Flying_Pokemon(Pokemon):
+    p_type = "Flying"
+
+    def __init__(self, name,level = 5):
+        self.name = name
+        self.level = level
+        self.weak = "Electric"
+        self.strong = "Fighting"
