6b78f87a06115715b9ec8d60ddc84695db429000
Quvonchbek Bobojonov <111079758+QuvonchbekBobojonov@users.noreply.github.com>
2024-01-14 15:42:11 +0500

    Update Advance functions.py

--- Python Functions, Files, and Dictionaries/week-4/Advance functions.py
+++ Python Functions, Files, and Dictionaries/week-4/Advance functions.py
@@ -55,8 +55,8 @@ def checkingIfIn(a, direction = True, d = {'apple': 2, 'pear': 1, 'fruit': 19, '
 '''Q-6 We have provided the function checkingIfIn such that if the first input parameter is in the third, dictionary, input parameter, then the function returns that value, and otherwise, it returns False.
 Follow the instructions in the active code window for specific variable assignmemts.'''
 
-def checkingIfIn(a, direction = True, d = {'apple': 2, 'pear': 1, 'fruit': 19, 'orange': 5, 'banana': 3, 'grapes': 2, 'watermelon': 7}):
-    if direction == True:
+def checkingIfIn(a, direction=True, d={'apple': 2, 'pear': 1, 'fruit': 19, 'orange': 5, 'banana': 3, 'grapes': 2, 'watermelon': 7}):
+    if direction:
         if a in d:
             return d[a]
         else:
@@ -68,4 +68,18 @@ def checkingIfIn(a, direction = True, d = {'apple': 2, 'pear': 1, 'fruit': 19, '
             return d[a]
 
 # Call the function so that it returns False and assign that function call to the variable c_false
-c_false = checkingIfIn('peas')
+c_false = checkingIfIn('Mango')
+print(c_false)
+
+# Call the function so that it returns True and assign it to the variable c_true
+c_true = checkingIfIn('Mango', False)
+print(c_true)
+
+# Call the function so that the value of fruit is assigned to the variable fruit_ans
+fruit_ans = checkingIfIn('fruit')
+print(fruit_ans)
+
+# Call the function using the first and third parameter so that the value 8 is assigned to the variable param_check
+param_check = checkingIfIn('pear')
+print(param_check)
+
